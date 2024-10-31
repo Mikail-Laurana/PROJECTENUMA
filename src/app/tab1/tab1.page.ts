e5ec8dfa6cc5540ab723d3bc68bb976be2fa9f3b
Mikail Laurana <lauaranamiko@gmail.com>
2024-10-31 13:20:37 +0700

    src/app/tab1/tab1.page.html

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  photoURL: string = 'assets/default-profile.png';
+  
   constructor() {}
 
+  uploadImage() {
+    const fileInput: HTMLElement =
+    document.getElementById('fileInput')!;
+    fileInput.click();
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files![0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.photoURL = reader.result as string;
+      }
+    }
+  }
 }
